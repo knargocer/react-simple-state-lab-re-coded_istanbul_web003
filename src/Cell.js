11d33d432777d51c2b0be39fb6af18cf6ce79912
Knar Gocer <knargr@gmail.com>
2020-11-01 17:48:31 +0000

    Automatically backed up by Learn

--- src/Cell.js
+++ src/Cell.js
@@ -14,7 +14,7 @@ export default class Cell extends Component {
     }
 
     render() {
-        return <div className="cell" style={{ backgroundColor: this.state.color }} onClick={this.clickToChangeColor}></div>
+        return <div className="cell" style={{ backgroundColor: this.state.color }} onClick={()=>this.setState({color:'#333'})></div>
     }
 }
 
